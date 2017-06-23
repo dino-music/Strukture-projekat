a09d4233d16c3cfb2a08948a42d54f80b6e25795
harun-muderizovic <harun.muderizovic@outlook.com>
2017-06-23 18:46:21 +0200

    Update utility_functions.h

--- utility_functions.h
+++ utility_functions.h
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "APIs.h"
+#include <sstream>
 
 bool isValid_mail(std::string mail);
 bool isValid_jmbg(std::string jmbg);
+void SubjTeach(subjectapi&, teacherapi&, const std::string&);
