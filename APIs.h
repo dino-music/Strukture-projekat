b006f0eb02282d35206a65b9f24b5509aab57fa3
dino-music <dino.music@fet.ba>
2017-06-23 19:55:45 +0200

    update

--- APIs.h
+++ APIs.h
@@ -3,22 +3,51 @@
 #include "API.h"
 #include "objekti.h"
 
+class studentapi;
+class teacherapi;
+class subjectapi;
+class departmentapi;
+
+
 class studentapi:public API<Student>
 {
+  private:
+    subjectapi* subjectAPI;
+    departmentapi* departmentAPI;
 
+  public:
+    void setSubjectAPI(subjectapi *reff){subjectAPI=reff;}
+    void setDepartmentAPI(departmentapi *reff){departmentAPI=reff;}
 };
 
 class teacherapi:public API<Teacher>
 {
+  private:
+    subjectapi* subjectAPI;
+    departmentapi* departmentAPI;
 
+  public:
+    void setSubjectAPI(subjectapi* reff){subjectAPI=reff;}
+    void setDepartmentAPI(departmentapi* reff){departmentAPI=reff;}
 };
 
 class subjectapi:public API<Subject>
 {
-
+  private:
+    departmentapi* departmentAPI;
+    studentapi* studentAPI;
+  
+  public:
+    void setDepartmentAPI(departmentapi* reff){departmentAPI=reff;}
+    void setStudentAPI(studentapi* reff){studentAPI=reff;}
 };
+
 
 class departmentapi:public API<Department>
 {
+  private:
+    subjectapi* subjectAPI;
 
+  public:
+    void setSubjectAPI(subjectapi* reff){subjectAPI=reff;}
 };
