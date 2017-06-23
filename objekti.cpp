947242dc9d3e8544d7cbf14b7d63a667f6fa3943
eminaMahmutbegovic <eminaa.mahmutbegovic@gmail.com>
2017-06-23 16:19:38 +0200

    Add files via upload

---
+++ objekti.cpp
@@ -0,0 +1,105 @@
+#include"objekti.h"
+//Emina Mahmutbegovic
+
+#include <stdexcept>
+#include"utility_functions.h"
+Student::Student(const std::string& line){ 
+  std::stringstream sline(line);
+  int i=1;
+  std::string info;
+  getline(sline,info,',');
+  id=stoi(info);
+  while(std::getline(sline,info,' ')){ 
+  getline(sline,info,',');
+  ++i;
+  switch(i){
+    case 2: firstName=info; break;
+    case 3: lastName=info; break;
+    case 4: birthDate=info; break;
+    case 5: if(!isValid_mail(info))
+              throw std::invalid_argument("pogresan unos maila");
+            email=info;
+            break;
+    case 6: gender=info[0]; break;
+    case 7: if(!isValid_jmbg(info))
+            throw std::invalid_argument("pogresan unos jmbg");
+            jmbg=info; break;
+    case 8: depId=stoi(info); break; 
+
+  }
+  }
+}
+
+void Student::print()const{
+  std::cout<<id<<" "<<firstName<<" "<<lastName<<" "<<birthDate<<" "<<email<<" "<<gender<<" "<<
+    jmbg<<" "<<depId<<" "<<std::endl;
+
+}
+
+
+Teacher::Teacher(const std::string& line): Student(){
+  std::stringstream sline(line);
+  int i=1;
+  std::string info;
+  getline(sline,info,',');
+  id=stoi(info);
+  while(std::getline(sline,info,' ')){
+  getline(sline,info,',');
+  ++i;
+  switch(i){
+    case 2: firstName=info; break;
+    case 3: lastName=info; break;
+    case 4: birthDate=info; break;
+    case 5: if(!isValid_mail(info))
+            throw std::invalid_argument("pogresan unos maila");
+            email=info;
+            break;
+    case 6: gender=info[0]; break;
+    case 7: if(!isValid_jmbg(info))
+            throw std::invalid_argument("pogresan unos jmbg");
+            jmbg=info; break;
+    case 8: title=info; break;
+    case 9: depId=stoi(info); break;
+  }
+  }  
+}
+
+void Teacher::print()const{
+   std::cout<<id<<" "<<firstName<<" "<<lastName<<" "<<birthDate<<" "<<email<<" "<<gender<<" "<<
+    jmbg<<" "<<title<<" "<<depId<<" "<<std::endl; 
+}
+
+Subject::Subject(const std::string& line){
+  std::stringstream sline(line);
+  std::string info;
+  std::getline(sline,info,',');
+  id=std::stoi(info);
+  int i=1;
+  while(std::getline(sline,info,' ')){
+  std::getline(sline,info,',');
+    ++i;
+    switch(i){
+      case 2: name=info; break;
+      case 3: ects=std::stoi(info); break;
+      case 4: abberv=info; break;
+    }
+}
+}
+
+void Subject::print()const{
+  std::cout<<id<<" "<<name<<" "<<ects<<" "<<abberv<<" "<<year<<" "<<semester<<std::endl;
+}
+
+Department::Department(const std::string& line){
+  std::stringstream sline(line);
+  std::string info; 
+  std::getline(sline,info,',');
+  id=std::stoi(info);
+  std::getline(sline,info,' ');
+  std::getline(sline,info,',');
+  name=info;
+}
+
+void Department::print()const{
+  std::cout<<id<<""<<name<<std::endl;
+}
---
+++ objekti.h
@@ -0,0 +1,76 @@
+//#pragma once
+#include<string>
+#include<sstream>
+#include <iostream>
+#include "linkedlist.h"
+//Emina Mahmutbegovic
+class Student{
+  protected:
+    unsigned int id;
+    std::string firstName;
+    std::string lastName;
+    std::string birthDate;
+    std::string email;
+    char gender;
+    std::string jmbg;
+    unsigned int depId;
+  linkedlist<unsigned int> subjects;
+  public:
+  Student()=default;
+  Student(const std::string&);
+  unsigned int getId()const{return id;}
+  const std::string& getFirstName()const {return firstName;}
+  const std::string& getLastName()const{return lastName;}
+  const std::string& getBirthDate()const{return birthDate;}
+  const std::string& getEmail()const{return email;} 
+  const std::string& getJmbg()const{return jmbg;}
+  char getGender()const{return gender;}
+  unsigned int getDepId()const{return depId;}
+  void print()const;
+  void addSubject(unsigned int id){subjects.push_back(id);}
+};
+
+class Teacher: public Student{
+  protected:
+  std::string title;
+  public:
+  Teacher(const std::string&);
+  void print()const;
+  const std::string& getTitle()const {return title;}
+};
+
+class Subject{
+  private:
+    unsigned int id;
+    std::string name;
+    unsigned int ects;
+    std::string abberv;
+    std::string year;
+    std::string semester;
+  unsigned int department;
+  
+  public:
+  Subject(const std::string&);
+  unsigned int getId()const{return id;}
+  void print()const;
+  void setYear(const std::string& y){year=y;}
+  void setSemester(const std::string& s){semester=s;}
+  void setDepartment(unsigned int dep){department=dep;}
+};
+
+
+class Department{
+  private:
+    unsigned int id;
+    std::string name;
+  linkedlist<unsigned int> subjects;
+  public:
+    Department(const std::string&);
+    unsigned int getId()const{return id;}
+    void print()const;
+    void addSubject(unsigned int id){subjects.push_back(id);}
+
+};
+
+
+
